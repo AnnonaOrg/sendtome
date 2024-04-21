4e8589b8ce706ec64132d6860f37da664203e1be
noreply <noreply@yourdomain.com>
2024-04-21 19:32:02 +0800

    v0.0.1 debug

--- _git_push.sh
+++ _git_push.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION=0.1.8
+VERSION=0.0.1
 APPNAME=telebot_app_serverless_sendtome
 echo "package constvar" > ./internal/constvar/version.go
 echo "const(APP_NAME = \"${APPNAME}\"" >> ./internal/constvar/version.go
@@ -9,13 +9,13 @@ go fmt ./internal/constvar
 
 
 rm ./main/main
-exit
+
 #git init #
 git add .
 git commit -m "v${VERSION} debug"
-git branch -M main #
-git push -u github main
+#git branch -M main #
+git push -u origin main
 
 git tag "v${VERSION}"
-git push --tags  -u github main
+git push --tags -u origin main
 
--- internal/constvar/version.go
+++ internal/constvar/version.go
@@ -2,5 +2,5 @@ package constvar
 
 const (
 	APP_NAME    = "telebot_app_serverless_sendtome"
-	APP_VERSION = "0.1.8"
+	APP_VERSION = "0.0.1"
 )
